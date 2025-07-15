7b7c2b0ac48a91bce5d3bd3f238be86b4b72a767
Monika2604 <155832211+Monika2604@users.noreply.github.com>
2025-07-15 21:46:45 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Java CI build!");
         System.out.println("This is new commit for testing CI");
+        System.out.println("This is new commit for testing2 CI");
     }
 }
